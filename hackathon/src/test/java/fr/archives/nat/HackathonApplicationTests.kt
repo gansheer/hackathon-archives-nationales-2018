da910d090111ad41726556ba83dc3f1eb25af14f
Patrick Allain <patrick.allain@soat.fr>
2018-12-07 13:15:38 +0100

    [CHORE] XML Bean generation !

---
+++ hackathon/src/test/java/fr/archives/nat/HackathonApplicationTests.kt
@@ -0,0 +1,16 @@
+package fr.archives.nat
+
+import org.junit.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+class HackathonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
