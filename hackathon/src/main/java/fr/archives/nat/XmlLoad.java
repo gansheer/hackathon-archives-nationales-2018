8d322594d4c078fb1ab240d92300c9c495b68c7a
Patrick Allain <patrick.allain@soat.fr>
2018-12-08 20:53:29 +0100

    [CHORE] Improve code

--- hackathon/src/main/java/fr/archives/nat/XmlLoad.java
+++ hackathon/src/main/java/fr/archives/nat/XmlLoad.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.sax.SAXSource;
 
@@ -32,7 +33,7 @@ public class XmlLoad<T> {
 
     private final Class<T> klass;
 
-    public XmlLoad(XmlFiles xml, Class<T> klass) {
+    public XmlLoad(final XmlFiles xml, final Class<T> klass) {
         this.xml = xml;
         this.klass = klass;
     }
@@ -45,13 +46,11 @@ public class XmlLoad<T> {
             return Stream.of(DtdFiles.values())
                     .filter(i -> i.getFileName().equals(expected))
                     .findFirst();
-        } catch (MalformedURLException | URISyntaxException e) {
-            throw new RuntimeException(e);
+        } catch (final MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException("Error during retrieve dtd for systemId : " + systemId, e);
         }
-
     }
 
-    @SuppressWarnings("unchecked")
     public T run() {
         try {
             final JAXBContext ctx = JAXBContext.newInstance(this.klass);
@@ -60,29 +59,31 @@ public class XmlLoad<T> {
             final XMLReader xmlReader = XMLReaderFactory.createXMLReader();
             xmlReader.setFeature("http://xml.org/sax/features/namespaces", true);
             xmlReader.setFeature("http://xml.org/sax/features/namespace-prefixes", true);
-            xmlReader.setEntityResolver(new EntityResolver() {
-
-                @Override
-                public InputSource resolveEntity(String arg0, String systemId) throws SAXException, IOException {
-                    final DtdFiles f = getDtd(systemId).orElseThrow(() -> new RuntimeException("Oups"));
-                    try (final FileInputStream fis =new FileInputStream(f.getFile())) {
-                        final String collect = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8))
-                                .lines()
-                                .collect(Collectors.joining());
-                        return new InputSource(new StringReader(collect));
-                    }
+            xmlReader.setEntityResolver((unused, systemId) -> {
+                final DtdFiles f = getDtd(systemId).orElseThrow(() -> new RuntimeException("DTD not found"));
+                try (final FileInputStream fis = new FileInputStream(f.getFile())) {
+                    final String collect = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8))
+                            .lines()
+                            .collect(Collectors.joining());
+                    return new InputSource(new StringReader(collect));
                 }
             });
 
-            try (final FileInputStream fis = new FileInputStream(xml.getFile())) {
-                InputSource inputSource = new InputSource(fis);
-                SAXSource source = new SAXSource(xmlReader, inputSource);
-
-                return (T) unmarshaller.unmarshal(source);
-            }
+            return loadObj(unmarshaller, xmlReader);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+
             throw new RuntimeException(e);
+        }
+    }
+
+    private T loadObj(final Unmarshaller unmarshaller, final XMLReader xmlReader) throws IOException, JAXBException {
+        try (final FileInputStream fis = new FileInputStream(xml.getFile())) {
+            final InputSource inputSource = new InputSource(fis);
+            final SAXSource source = new SAXSource(xmlReader, inputSource);
+
+            @SuppressWarnings("unchecked")
+            final T obj = (T) unmarshaller.unmarshal(source);
+            return obj;
         }
     }
 
@@ -92,12 +93,9 @@ public class XmlLoad<T> {
 
         FRAN_IR_056870("data/FRAN_IR_056870.xml");
 
-        private String location;
-
-        private File file;
+        private final File file;
 
         XmlFiles(String location) {
-            this.location = location;
             this.file = Paths.get("src/main/resources/" + location).toAbsolutePath().normalize().toFile();
         }
 
@@ -105,33 +103,22 @@ public class XmlLoad<T> {
             return file;
         }
 
-        public String getLocation() {
-            return location;
-        }
-
     }
 
     enum DtdFiles {
         EAD_SIA("ead_sia.dtd", "toolbox/dtd_an/EAD/");
 
-        private String fileName;
+        private final String fileName;
 
-        private String folder;
-
-        private File file;
+        private final File file;
 
         DtdFiles(String fileName, String folder) {
             this.fileName = fileName;
-            this.folder = folder;
             this.file = Paths.get("src/main/resources/" + folder + fileName).toAbsolutePath().normalize().toFile();
         }
 
         public String getFileName() {
             return fileName;
-        }
-
-        public String getFolder() {
-            return folder;
         }
 
         public File getFile() {
