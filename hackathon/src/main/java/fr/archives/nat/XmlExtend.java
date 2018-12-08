7381057743779cc371262e1ff84808afd6c18b78
thermo <romain.chauveau@protonmail.com>
2018-12-08 21:22:30 +0100

    add model + init persing

--- hackathon/src/main/java/fr/archives/nat/XmlExtend.java
+++ hackathon/src/main/java/fr/archives/nat/XmlExtend.java
@@ -1,6 +1,11 @@
 package fr.archives.nat;
 
+import java.util.List;
+
 import fr.archives.nat.XmlLoad.XmlFiles;
+import fr.archives.nat.model.Decret;
+import fr.archives.nat.model.Person;
+import fr.archives.nat.xml.ead.sia.C;
 import fr.archives.nat.xml.ead.sia.Ead;
 
 public class XmlExtend {
@@ -9,6 +14,34 @@ public class XmlExtend {
 		final XmlFiles source = XmlFiles.FRAN_IR_056040;
 		XmlLoad<Ead> xmlLoader = new XmlLoad<>(source, Ead.class);
 		Ead ead = xmlLoader.run();
+		List<C> decrets = ead.getArchdesc().getDsc().getC();
+		
+		for(C decret : decrets) {
+			
+			Decret decretModel = extractDecret(decret);
+			// bla bla
+			
+			List<C> decretPersons  = decret.getC();
+			
+			for(C decretPerson : decretPersons) {
+				
+				List<Person> persons = extractPersons(decretPerson,decret);
+				
+				
+			}
+		}
+		
 		System.out.println(ead.toString());
+	}
+
+	private static List<Person> extractPersons(C decretPerson, C decret) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Decret extractDecret(C decret) {
+		Decret decretModel = new Decret();
+		//decretModel.setNumDocument(decret.getDid().getUnitid().stream().filter(unitid -> ));
+		return decretModel;
 	}	
 }
---
+++ hackathon/src/main/java/fr/archives/nat/model/Person.java
@@ -0,0 +1,139 @@
+package fr.archives.nat.model;
+
+public class Person {
+
+	private Decret decret;
+	
+	private String numDossierNat;
+	
+	private String nom;
+	
+	private String nomNaissance;
+	
+	private String prenom;
+	
+	// date
+	private String dataNaissance;
+	private String dataNaissanceJour;
+	private String dataNaissanceMois;
+	private String dataNaissanceAnnee;
+	private String lieuNaissanceCommune;
+	private String lieuNaissanceDepartement;
+	private String lieuNaissancePays;
+	private String origineParent;
+	private String origineParentPays;
+	private String profession;
+	private String lieuResidenceCommune;
+	private String lieuResidenceDepartement;
+	private String lieuResidencePays;
+	public Decret getDecret() {
+		return decret;
+	}
+	public void setDecret(Decret decret) {
+		this.decret = decret;
+	}
+	public String getNumDossierNat() {
+		return numDossierNat;
+	}
+	public void setNumDossierNat(String numDossierNat) {
+		this.numDossierNat = numDossierNat;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getNomNaissance() {
+		return nomNaissance;
+	}
+	public void setNomNaissance(String nomNaissance) {
+		this.nomNaissance = nomNaissance;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getDataNaissance() {
+		return dataNaissance;
+	}
+	public void setDataNaissance(String dataNaissance) {
+		this.dataNaissance = dataNaissance;
+	}
+	public String getDataNaissanceJour() {
+		return dataNaissanceJour;
+	}
+	public void setDataNaissanceJour(String dataNaissanceJour) {
+		this.dataNaissanceJour = dataNaissanceJour;
+	}
+	public String getDataNaissanceMois() {
+		return dataNaissanceMois;
+	}
+	public void setDataNaissanceMois(String dataNaissanceMois) {
+		this.dataNaissanceMois = dataNaissanceMois;
+	}
+	public String getDataNaissanceAnnee() {
+		return dataNaissanceAnnee;
+	}
+	public void setDataNaissanceAnnee(String dataNaissanceAnnee) {
+		this.dataNaissanceAnnee = dataNaissanceAnnee;
+	}
+	public String getLieuNaissanceCommune() {
+		return lieuNaissanceCommune;
+	}
+	public void setLieuNaissanceCommune(String lieuNaissanceCommune) {
+		this.lieuNaissanceCommune = lieuNaissanceCommune;
+	}
+	public String getLieuNaissanceDepartement() {
+		return lieuNaissanceDepartement;
+	}
+	public void setLieuNaissanceDepartement(String lieuNaissanceDepartement) {
+		this.lieuNaissanceDepartement = lieuNaissanceDepartement;
+	}
+	public String getLieuNaissancePays() {
+		return lieuNaissancePays;
+	}
+	public void setLieuNaissancePays(String lieuNaissancePays) {
+		this.lieuNaissancePays = lieuNaissancePays;
+	}
+	public String getOrigineParent() {
+		return origineParent;
+	}
+	public void setOrigineParent(String origineParent) {
+		this.origineParent = origineParent;
+	}
+	public String getOrigineParentPays() {
+		return origineParentPays;
+	}
+	public void setOrigineParentPays(String origineParentPays) {
+		this.origineParentPays = origineParentPays;
+	}
+	public String getProfession() {
+		return profession;
+	}
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+	public String getLieuResidenceCommune() {
+		return lieuResidenceCommune;
+	}
+	public void setLieuResidenceCommune(String lieuResidenceCommune) {
+		this.lieuResidenceCommune = lieuResidenceCommune;
+	}
+	public String getLieuResidenceDepartement() {
+		return lieuResidenceDepartement;
+	}
+	public void setLieuResidenceDepartement(String lieuResidenceDepartement) {
+		this.lieuResidenceDepartement = lieuResidenceDepartement;
+	}
+	public String getLieuResidencePays() {
+		return lieuResidencePays;
+	}
+	public void setLieuResidencePays(String lieuResidencePays) {
+		this.lieuResidencePays = lieuResidencePays;
+	}
+	
+	
+}
